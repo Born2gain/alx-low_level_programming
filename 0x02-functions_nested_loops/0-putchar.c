2045d644670304f830f4c5adfbc88560e3425983
Daniel <afolayandaniel8@gmail.com>
2022-06-07 18:20:13 -0700

    initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,10 +10,10 @@ int main (void)
 	int n = 0;
 	char name[8] = "_putchar";
 	while (n < 8)
-	{
+		{
 		_putchar(name[n]);
 		n++;
-	}
-	_putchar('\n');
+		}
+		_putchar('\n');
 	return 0;
 }
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *prints _putchar followed by a new line
+ *
+ * return 0 on success
+ */
+int main (void)
+{
+        int n = 0;
+        char name[8] = "_putchar";
+        while (n < 8)
+                {                                        putchar(name[n]);
+                n++;                                     }                                        putchar('\n');
+        return 0;                        }

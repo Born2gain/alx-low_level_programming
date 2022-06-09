731adf6cefe876b6db2068501316416fd5e3736d
Daniel <afolayandaniel8@gmail.com>
2022-06-09 16:44:10 -0700

    initial commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,22 @@
-include "main.h"
+#include "main.h"
 
 /**
- * _isupper - check if the argument is upper case
- *
- * Description: print out the required result
- *
- * Return: return 0
+ *_isupper - checks the code
+ *@c: stores the letters
+ *Return: Returns a value of 1 if it contains an uppercase
  *
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	} else
-		return (0);
+
+        if (c >= 'A' && c <= 'Z')
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+
 }

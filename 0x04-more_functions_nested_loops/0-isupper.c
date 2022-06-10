efc0e4626199557d524f90747ba1f1f2df5d0f7b
Daniel <afolayandaniel8@gmail.com>
2022-06-09 17:23:16 -0700

    initial

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - runs the code
+ * @c: Argument passed to the function
+ *
+ * Description: print out the result
+ *
+ * Return: return 0
+ *
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	} else
+		return (0);
+}

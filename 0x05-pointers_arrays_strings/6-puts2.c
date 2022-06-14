33e68e39059494f271d2f291741a5f0d883890ff
Daniel <afolayandaniel8@gmail.com>
2022-06-14 16:01:46 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,16 +9,16 @@
 
 void puts2(char *str)
 {
-        int i, j;
-	
+	int i;
+	int j;
+
 	for (i = 0; str[i] != '\0'; i++)
-        {
-                ;
-        }
-	
-        for (j = 0; j < i; j += 2)
-        {
-                _putchar(str[j]);
-        }
-        _putchar(10);
+	{
+		;
+	}
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar(10);
 }

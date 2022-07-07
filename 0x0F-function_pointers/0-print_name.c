b1a45b3edd29354d2a22cfdf3fa224292edc2868
Daniel <afolayandaniel8@gmail.com>
2022-07-06 17:01:43 -0700

    initial

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,7 +7,7 @@ include <stdio.h>
  *
  * Return: return void
  */
-void print_name(char *name, void(*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
